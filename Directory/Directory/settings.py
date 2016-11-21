a81decdb00207f25a77e391d42c293c85e1cfa90
Karan Sangha <iamkaransangha@gmail.com>
2016-11-20 19:41:47 -0800

    debug false, allowed hosts empty

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = '8c%$z*q-4$))l9h0t6x5+sfd5xacmy-)^zqh#a^!3t$mj59d20'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '*',
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
