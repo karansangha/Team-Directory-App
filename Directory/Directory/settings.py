4e9b52db25e5a5f13e5ebd70e2bb914868f857f1
Karan Sangha <iamkaransangha@gmail.com>
2016-11-20 19:07:57 -0800

    allowed hosts update

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '8c%$z*q-4$))l9h0t6x5+sfd5xacmy-)^zqh#a^!3t$mj59d20'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    https://evening-brushlands-22749.herokuapp.com/
+]
 
 
 # Application definition
