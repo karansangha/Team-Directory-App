850e9bb3b5a866189b7aef35e847ee420ad550ef
Karan Sangha <iamkaransangha@gmail.com>
2016-11-22 15:58:31 -0800

    Added a form for editing user profiles
    Also updated models to have more data fields: responsibilities, bio,
    twitter, github and birthday.

--- Directory/www/views.py
+++ Directory/www/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Person
+from .forms import PersonForm
 # Create your views here.
 
 def index(request):
@@ -9,3 +10,7 @@ def index(request):
 def detail(request, slug):
     person = Person.objects.get(slug=slug)
     return render(request, 'detail.html', {'person':person})
+
+def edit(request, slug):
+    form = PersonForm()
+    return render(request, 'edit.html', {'form': form})
